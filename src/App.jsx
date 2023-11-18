02857016a9f5359812dddd4ad5f43cb18ba82a41
Yusuf-oluwaseyi <tobacrown@gmail.com>
2023-11-18 18:02:04 +0100

    navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-
-import './App.css'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <div className='bg-purple-500'>
-        <h1 className="text-xl">Hello</h1>
-        <button className='btn'>Click</button>
-      </div>
-    </>
-  )
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar title="Github Finder"/>
+
+          <main>Content</main>
+        </div>
+      </Router>
+  );
 }
 
-export default App
+export default App;
