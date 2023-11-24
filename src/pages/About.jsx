a314b27c517a1a54d7df46375238eaa02355a054
Yusuf-oluwaseyi <tobacrown@gmail.com>
2023-11-24 11:34:38 +0100

    footer and about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,11 @@
 function About() {
   return (
     <div>
-      About
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-dark">A React app to search Github profiles and see profile details, This project was create by Adejimi Levi</p>
+      <p className="text-lg text-dark-500">
+        Version <span className="text-green">2.5.0</span>
+      </p>
     </div>
   )
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,20 @@
-
+import { FaHome } from "react-icons/fa"
+import { Link } from "react-router-dom"
+import wall from "../assets/wall-e.jpg";
 
 function NotFound() {
   return (
-    <div>
-      Not Found
+    <div className="hero">
+      <div className="text-center hero-content">
+        <div className="max-w-lg">
+          <h1 className="text-8xl font-bold mb-8">Oooops!</h1>
+          <p className="text-2xl mb-8 font-bold">404 - WE LOST THIS PAGE, AGAIN!!</p>
+          <Link to='/' className="btn btn-[#fdffb8] btn-lg">
+            <FaHome className="mr-2" />
+            Back Home
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
